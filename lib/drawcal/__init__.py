0b632297779146ce81f4cfa8c3b1859ba8df48c6
Ryan Galloway <ryan@rsg.io>
2022-08-29 18:27:49 -0700

    fixes missing ttf file, adds help strings to cli tool, v0.5.1

--- lib/drawcal/__init__.py
+++ lib/drawcal/__init__.py
@@ -5,9 +5,14 @@ Draws calendars and events.
 """
 
 __prog__ = "drawcal"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 __author__ = "ryan@rsg.io"
 
 __todo__ = """
 - support json file with list of event dicts with start/end dates
 """
+
+import os
+
+LIBDIR = os.path.dirname(os.path.abspath(__file__))
+ARIAL_TTF_FILE = os.path.join(LIBDIR, "arial.ttf")
--- lib/drawcal/drawlib.py
+++ lib/drawcal/drawlib.py
@@ -8,6 +8,8 @@ import calendar
 from datetime import datetime, timedelta
 from PIL import Image, ImageFont, ImageDraw
 
+from drawcal import ARIAL_TTF_FILE
+
 # set some global date values
 _d = datetime.today()
 today_str = f"{_d.month}/{_d.day}/{_d.year}"
@@ -69,7 +71,7 @@ def draw_calendar(month=today.month, year=today.year, events=None,
     draw = ImageDraw.Draw(img)
 
     # draw the month and year
-    font = ImageFont.truetype("arial", size=15)
+    font = ImageFont.truetype(ARIAL_TTF_FILE, size=15)
     header_w, header_h = draw.textsize(header, font=font)
     draw.text(((width-header_w)/2, pad/2), header,
         fill=colors.title_text, font=font)
@@ -248,7 +250,7 @@ def draw_calendar(month=today.month, year=today.year, events=None,
                 draw.rectangle((x1, y1-1, x1+24, y1+25), outline=colors.highlight)
 
             # draw days of the week and date numbers
-            col_font = ImageFont.truetype("arial", size=12)
+            col_font = ImageFont.truetype(ARIAL_TTF_FILE, size=12)
             col_w, col_h = draw.textsize(col, font=col_font)
 
             # set date text color
